d533f3dce9cc3040debea5fd43cd5648dc9c764a
tregzmusic <tregzmusic@gmail.com>
2022-08-28 22:12:08 +0300

    finish

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+name_app = MainappConfig.name
+
+urlpatterns = [
+    path("", views.MainPageView.as_view()),
+    path("news/", views.NewsPageView.as_view()),
+    path("courses/", views.CoursesPageView.as_view()),
+    path("contacts/", views.ContactsPageView.as_view()),
+    path("doc_site/", views.DocSitePageView.as_view()),
+    path("login/", views.LoginPageView.as_view()),
+]
