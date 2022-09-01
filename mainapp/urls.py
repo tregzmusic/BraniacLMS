620de3184cd768a9a8713fdf7bf3e4ce2011d468
tregzmusic <tregzmusic@gmail.com>
2022-09-01 12:17:25 +0300

    lesson_3. Маршрутизация в шаблонах

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,10 +7,10 @@ from mainapp.apps import MainappConfig
 name_app = MainappConfig.name
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("doc_site/", views.DocSitePageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="index"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
