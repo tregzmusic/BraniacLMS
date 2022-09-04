8e0a4f02935a6559acd03fcb7d9dd8b5fd83f0c6
tregzmusic <tregzmusic@gmail.com>
2022-09-04 15:24:40 +0300

    lesson_3. End_Lesson

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,11 +4,13 @@ from django.urls import path
 from mainapp import views
 from mainapp.apps import MainappConfig
 
-name_app = MainappConfig.name
+app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="index"),
     path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(),
+         name="news_paginator"),
     path("courses/", views.CoursesPageView.as_view(), name="courses"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
--- mainapp/views.py
+++ mainapp/views.py
@@ -20,6 +20,14 @@ class NewsPageView(TemplateView):
         context["data_obj"] = datetime.now()
         return context
 
+class NewsWithPaginatorView(NewsPageView):
+
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
+
+
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses.html"
 
